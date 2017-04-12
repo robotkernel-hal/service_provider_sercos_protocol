3198ef631652fbf6961004cb91793b6c7e89d537
Robert Burger <robert.burger@dlr.de>
2017-04-12 09:54:19 +0200

    fixed template args

--- src/main/cpp/provider.h
+++ src/main/cpp/provider.h
@@ -42,7 +42,7 @@ namespace service_provider {
         // forward declaration
         class handler;
 
-        class provider : public robotkernel::service_provider_base<handler> {
+        class provider : public robotkernel::service_provider_base<handler, base> {
             public:
                 //! default construction
                 /*!
