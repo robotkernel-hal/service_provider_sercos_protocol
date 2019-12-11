e84a0b51d4781c2a44cb5ee1645c2b90e68f9d31
Robert Burger <robert.burger@dlr.de>
2019-12-11 12:44:23 +0100

    using conan template

--- conanfile.py
+++ conanfile.py
@@ -1,42 +1,10 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
-import re, os
+from conans import tools, python_requires
 
-class MainProject(ConanFile):
+base = python_requires("conan_template/[~=5]@robotkernel/stable")
+
+class MainProject(base.RobotkernelConanFile):
     name = "service_provider_sercos_protocol"
-    license = "GPLv3"
-    url = f"https://rmc-github.robotic.dlr.de/robotkernel/{name}"
     description = "robotkernel-5 service provider for sercos protocol devices."
-    settings = "os", "compiler", "build_type", "arch"
     exports_sources = ["*", "!.gitignore"] + ["!%s" % x for x in tools.Git().excluded_files()]
-    generators = "pkg_config"
     requires = "robotkernel/[~=5.0]@robotkernel/stable"
 
-    def source(self):
-        filedata = None
-        filename = "project.properties"
-        with open(filename, 'r') as f:
-            filedata = f.read()
-        with open(filename, 'w') as f:
-            f.write(re.sub("VERSION *=.*[^\n]", f"VERSION = {self.version}", filedata))
-
-    def build(self):
-        self.run("autoreconf -if")
-        autotools = AutoToolsBuildEnvironment(self)
-        autotools.libs=[]
-        autotools.include_paths=[]
-        autotools.library_paths=[]
-        if self.settings.build_type == "Debug":
-            autotools.flags = ["-O0", "-g"]
-        else:
-            autotools.flags = ["-O3"]
-        autotools.configure(configure_dir=".")
-        autotools.make()
-
-    def package(self):
-        autotools = AutoToolsBuildEnvironment(self)
-        autotools.install()
-
-    def package_info(self):
-        self.cpp_info.includedirs = ['include']
-        self.cpp_info.bindirs = ['bin']
-        self.cpp_info.resdirs = ['share']
