b1f61f02d12d3989194c649c4bd7c9db6bf78fcc
Johannes Nix <johannes.nix@dlr.de>
2022-12-01 16:34:03 +0100

    fix: add missing logger

--- bindings/python/rk_gtk3_gui_plugin/service_provider_sercos_protocol/sercos_view.py
+++ bindings/python/rk_gtk3_gui_plugin/service_provider_sercos_protocol/sercos_view.py
@@ -30,6 +30,9 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 #from gi.repository import GObject
 
+import logging
+logger = logging.getLogger()
+logger.debug("loading sercos_view module")
 
 import helpers
 
