02bd71d482aa7705da6d5f5fcad21ed9d9d22ffa
Florian Schmidt <florian.schmidt@dlr.de>
2022-12-12 10:49:56 +0100

    add missing import from gi

--- bindings/python/rk_gtk3_gui_plugin/service_provider_sercos_protocol/sercos_view.py
+++ bindings/python/rk_gtk3_gui_plugin/service_provider_sercos_protocol/sercos_view.py
@@ -28,6 +28,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 #gi.require_version('GLib', '2.0')
 from gi.repository import Gtk
+from gi.repository import GLib
 #from gi.repository import GObject
 
 import logging
