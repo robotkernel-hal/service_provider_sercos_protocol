d6e29b445b2a89bcc5fb59f91ff28ac06129357d
Robert Burger <robert.burger@dlr.de>
2023-08-03 13:19:34 +0200

    using new service_helper classes

--- src/main/cpp/provider.cpp
+++ src/main/cpp/provider.cpp
@@ -39,62 +39,25 @@ using namespace robotkernel;
 using namespace service_provider;
 using namespace string_util;
 
-const std::string service_provider::sercos_protocol::handler::service_definition_read_id = robotkernel_service_provider_sercos_protocol_read_id_service_definition;
-const std::string service_provider::sercos_protocol::handler::service_definition_write_id = robotkernel_service_provider_sercos_protocol_write_id_service_definition;
-
-
-#if defined __VXWORKS__ || defined __QNX__
-#ifndef min
-#define min(a, b) ((a) < (b) ? (a) : (b))
-#endif
-
-#define strnlen(a, b) \
-    min(strlen((a)), (b))
-#endif
-
 //! handler construction
 sercos_protocol::handler::handler(const robotkernel::sp_service_interface_t& req) 
-    : log_base(req->owner, "sercos_protocol", req->device_name) {
-    robotkernel::kernel& k = *robotkernel::kernel::get_instance();
-
+    : log_base(req->owner, "sercos_protocol", req->device_name) 
+{
     _instance = std::dynamic_pointer_cast<service_provider::sercos_protocol::base>(req);
     if (!_instance)
         throw str_exception("wrong base class");
 
-    k.add_service(_instance->owner, _instance->device_name + ".read_id", 
-            service_definition_read_id,
-            std::bind(&sercos_protocol::handler::service_read_id, this, _1, _2));
-    k.add_service(_instance->owner, _instance->device_name + ".write_id", 
-            service_definition_write_id,
-            std::bind(&sercos_protocol::handler::service_write_id, this, _1, _2));
+    add_svc_read_id(_instance->owner, _instance->device_name + ".read_id");
+    add_svc_write_id(_instance->owner, _instance->device_name + ".write_id");
 }
 
-//! handler destruction
-sercos_protocol::handler::~handler() {
-    kernel& k = *kernel::get_instance();
-    k.remove_service(_instance->owner, _instance->device_name + ".read_id");
-    k.remove_service(_instance->owner, _instance->device_name + ".write_id");
-};
-
-//! service callback request read id
+//! svc_read_id
 /*!
- * \param request service request data
- * \parma response service response data
- * \return success
+ * \param[in]   req     Service request data.
+ * \param[out]  resp    Service response data.
  */
-int sercos_protocol::handler::service_read_id(const robotkernel::service_arglist_t& request, 
-        robotkernel::service_arglist_t& response) {
-#define READ_ID_REQ_IDN			0
-#define READ_ID_REQ_ELEMENTS	1
-    uint16_t idn     = request[READ_ID_REQ_IDN];
-    uint8_t elements = request[READ_ID_REQ_ELEMENTS];
-
-    // sercos service transfer data
-    service_id id;
-
-    // default response values 
-    string error_message = "", value = "", min_value = "N/A", max_value = "N/A";
-    uint32_t attr = 0;
+void sercos_protocol::handler::svc_read_id(const struct svc_req_read_id& req, struct svc_resp_read_id& resp) {
+    uint8_t elements = req.elements;
 
     if (    (elements & SSE_MINVAL) ||
             (elements & SSE_MAXVAL) ||
@@ -102,124 +65,74 @@ int sercos_protocol::handler::service_read_id(const robotkernel::service_arglist
         elements |= SSE_ATTR;   // we have to read attribute to interpret data
 
     try {
-        _instance->sercos_read_idn(idn, elements, id.data);
+        service_id id;
+        _instance->sercos_read_idn(req.idn, elements, id.data);
     
         if (elements & SSE_ATTR) 
-            attr = *(uint32_t *)&id.data.attr;
+            resp.attr = *(uint32_t *)&id.data.attr;
 
         if (    (elements & SSE_MINVAL) &&
                 (id.data.attr.datatype != SSA_DATATYPE_CHARSET))
-            min_value = id.min_val_to_string();
+            resp.min_value = id.min_val_to_string();
 
         if (    (elements & SSE_MAXVAL) &&
                 (id.data.attr.datatype == SSA_DATATYPE_CHARSET))
-            max_value = id.max_val_to_string();
+            resp.max_value = id.max_val_to_string();
 
         if (elements & SSE_DATA)
-            value = id.val_to_string();
+            resp.value = id.val_to_string();
     } catch (std::exception& e) {
-        error_message = e.what();
+        resp.error_message = e.what();
     }
-
-#define READ_ID_RESP_STRUCTURE		0
-#define READ_ID_RESP_NAME			1
-#define READ_ID_RESP_UNIT			2
-#define READ_ID_RESP_ATTR			3
-#define READ_ID_RESP_MIN_VALUE		4
-#define READ_ID_RESP_MAX_VALUE		5
-#define READ_ID_RESP_VALUE			6
-#define READ_ID_RESP_ERROR_MESSAGE	7
-    response.resize(8);
-    response[READ_ID_RESP_STRUCTURE]     = id.data.structure;
-    response[READ_ID_RESP_NAME]          = id.data.name;
-    response[READ_ID_RESP_UNIT]          = id.data.unit;
-    response[READ_ID_RESP_ATTR]          = attr;
-    response[READ_ID_RESP_MIN_VALUE]     = min_value;
-    response[READ_ID_RESP_MAX_VALUE]     = max_value;
-    response[READ_ID_RESP_VALUE]         = value;
-    response[READ_ID_RESP_ERROR_MESSAGE] = error_message;
-
-    return 0;
 }
 
-//! service callback request write id
+
+//! svc_write_id
 /*!
- * \param request service request data
- * \parma response service response data
- * \return success
+ * \param[in]   req     Service request data.
+ * \param[out]  resp    Service response data.
  */
-int sercos_protocol::handler::service_write_id(const robotkernel::service_arglist_t& request, 
-        robotkernel::service_arglist_t& response) {
-#define WRITE_ID_REQ_IDN		0
-#define WRITE_ID_REQ_ELEMENTS	1
-#define WRITE_ID_REQ_STRUCTURE	2
-#define WRITE_ID_REQ_NAME		3
-#define WRITE_ID_REQ_UNIT		4
-#define WRITE_ID_REQ_ATTR		5
-#define WRITE_ID_REQ_MIN_VALUE	6
-#define WRITE_ID_REQ_MAX_VALUE  7
-#define WRITE_ID_REQ_VALUE      8
-    uint16_t idn        = request[WRITE_ID_REQ_IDN];
-    uint8_t elements    = request[WRITE_ID_REQ_ELEMENTS];
-    uint16_t structure  = request[WRITE_ID_REQ_STRUCTURE];
-    string name         = request[WRITE_ID_REQ_NAME];
-    string unit         = request[WRITE_ID_REQ_UNIT];
-    uint32_t attr       = request[WRITE_ID_REQ_ATTR];
-    string min_value    = request[WRITE_ID_REQ_MIN_VALUE];
-    string max_value    = request[WRITE_ID_REQ_MAX_VALUE];
-    string value        = request[WRITE_ID_REQ_VALUE];
-
-    // response 
-    string error_message = "";
-
+void sercos_protocol::handler::svc_write_id(const struct svc_req_write_id& req, struct svc_resp_write_id& resp) {
     service_id id;    
 
-    if (elements & SSE_NAME) {
+    if (req.elements & SSE_NAME) {
         // TODO
     }
 
     // read structure
-    if (elements & SSE_STRC)
-        id.data.structure = structure;
+    if (req.elements & SSE_STRC)
+        id.data.structure = req.structure;
 
     // read unit
-    if (elements & SSE_UNIT) {
+    if (req.elements & SSE_UNIT) {
         // TODO
     }
 
     // get idn attribute
-    if (elements & SSE_ATTR)
-        id.data.attr = *(sercos_service_attribute *)&attr;
+    if (req.elements & SSE_ATTR)
+        id.data.attr = *(sercos_service_attribute *)&req.attr;
     else {
         try {
-            _instance->sercos_read_idn(idn, SSE_ATTR, id.data);
+            _instance->sercos_read_idn(req.idn, SSE_ATTR, id.data);
         } catch (std::exception& e) {
-            error_message = e.what();
-            goto func_exit;
+            resp.error_message = e.what();
+            return;
         }
     }
 
-    if (elements & SSE_MINVAL)
-        id.string_to_min_val(min_value.c_str());
+    if (req.elements & SSE_MINVAL)
+        id.string_to_min_val(req.min_value.c_str());
 
-    if (elements & SSE_MAXVAL)
-        id.string_to_max_val(max_value.c_str());
+    if (req.elements & SSE_MAXVAL)
+        id.string_to_max_val(req.max_value.c_str());
 
-    if (elements & SSE_DATA)
-        id.string_to_val(value.c_str());
+    if (req.elements & SSE_DATA)
+        id.string_to_val(req.value.c_str());
 
     try {
-        _instance->sercos_write_idn(idn, elements, id.data);
+        _instance->sercos_write_idn(req.idn, req.elements, id.data);
     } catch (std::exception& e) {
-        error_message = e.what();
+        resp.error_message = e.what();
     }
-
-func_exit:
-
-#define WRITE_ID_RESP_ERROR_MESSAGE  0
-    response.resize(1);
-    response[WRITE_ID_RESP_ERROR_MESSAGE] = error_message;
-
-    return 0;
 }
 
