309960971af7983b9ee32ddd8805440877baab62
Robert Burger <robert.burger@dlr.de>
2025-11-11 14:48:39 +0100

    fix: rk gui plugin path

--- bindings/python/conanfile.py
+++ bindings/python/conanfile.py
@@ -1,6 +1,7 @@
 import os
 from conan import ConanFile, conan_version
 from conan.tools.scm import Version
+from conan.tools.files import copy
 
 
 class service_provider_sercos_protocol_rkgui(ConanFile):
@@ -12,17 +13,17 @@ class service_provider_sercos_protocol_rkgui(ConanFile):
     url = f"https://rmc-github.robotic.dlr.de/robotkernel/service_provider_sercos_protocol"
     settings = "os"
     pure_python_folder = "."
-    exports_sources = os.path.join(pure_python_folder, "*")
+    exports_sources = [ "*", os.path.join(pure_python_folder, "*") ]
 
     def requirements(self):
         self.requires(f"service_provider_sercos_protocol_ln_msgdef/{self.version}@{self.user}/{self.channel}")
 
     def package(self):
-        self.copy(os.path.join(self.pure_python_folder, "*"))
+        copy(self, "*", self.source_folder, self.package_folder)
 
     def package_info(self):
-        pypath1 = os.path.join(self.package_folder, os.path.dirname(self.pure_python_folder))
-        pypath2 = os.path.join(self.package_folder, self.pure_python_folder)
+        pypath1 = os.path.join(self.package_folder, "rk_gtk3_gui_plugin")
+        pypath2 = os.path.join(self.package_folder, "rk_gui_plugin")
         if Version(conan_version) < "2.0.0":
             self.env_info.PYTHONPATH.append(pypath1)
             self.env_info.PYTHONPATH.append(pypath2)
