638733aeba3031adad3e383c1c89cc970305c1a1
Maximilian Mühlbauer <maximilian.muehlbauer@dlr.de>
2026-02-24 10:25:25 +0100

    fix: rkgui on ubuntu

--- bindings/python/rk_gtk3_gui_plugin/module_sercos/sercos_param_subview.py
+++ bindings/python/rk_gtk3_gui_plugin/module_sercos/sercos_param_subview.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-from __future__ import absolute_import
-
-from future import standard_library
-standard_library.install_aliases()
 import logging
 import os
 import sys
