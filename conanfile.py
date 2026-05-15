c0878a4e67aab9a5bcf5d32946022efe90df701f
Robert Burger <robert.burger@dlr.de>
2026-05-15 07:08:04 +0200

    change: conan_template stable, robotkernel_generator stable

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 import os
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[~6]@robotkernel/unstable"
+    python_requires = "conan_template/[~6]@robotkernel/stable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "service_provider_sercos_protocol"
@@ -10,7 +10,7 @@ class MainProject(ConanFile):
     description = "robotkernel service provider for sercos protocol devices."
     exports_sources = ["*", "!.gitignore", "!bindings"]
 
-    tool_requires = ["robotkernel_generator/[~6]@robotkernel/unstable"]
+    tool_requires = ["robotkernel_generator/[~6]@robotkernel/stable"]
 
     def requirements(self):
         self.requires(f"{self.name}_ln_msgdef/{self.version}@{self.user}/{self.channel}")
